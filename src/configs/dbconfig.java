aee499a8c5f1ecea93d23c9bf8e0ca8ee2e775fa
tejojr <ammar.fiky@gmail.com>
2019-10-02 20:46:58 +0700

    add con and func

--- src/configs/dbconfig.java
+++ src/configs/dbconfig.java
@@ -4,11 +4,27 @@
  * and open the template in the editor.
  */
 package configs;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author Zeref
  */
 public class dbconfig {
+    public static Connection con;
+    public static Connection Con()throws SQLException{
+        try {
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            con = DriverManager.getConnection("jdbc:mysql://localhost/faztservice","root","");
+            System.out.println("Koneksi Sukses");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return con;
+    }
+//    public static void main(String[] args) throws SQLException {
+//        System.out.println(Con());
+//    }
     
 }
--- src/configs/functions.java
+++ src/configs/functions.java
@@ -4,11 +4,51 @@
  * and open the template in the editor.
  */
 package configs;
-
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Zeref
  */
 public class functions {
+ public ResultSet rs;
+    public Statement st;
+    public int cud (String query, String info ) {
+        try {
+            String sql = query;
+            st = dbconfig.Con().createStatement();
+            st.executeUpdate(sql);
+            JOptionPane.showMessageDialog(null, info, "Informasi",JOptionPane.INFORMATION_MESSAGE);
+                
+            return 1;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e, "Informasi",JOptionPane.WARNING_MESSAGE);    
+            return 0;
+        } finally{
+            try {
+                dbconfig.Con().close();
+                
+            } catch (Exception e) {
+            }
+}
+    }
+       public int select (String query) {
+          try {
+            String sql = query;
+            st = dbconfig.Con().createStatement();
+            rs = st.executeQuery(sql);
+            return 1;
+        } catch (Exception e) {
+             JOptionPane.showMessageDialog(null, e, "Informasi",JOptionPane.WARNING_MESSAGE);    
+            return 0;
+        } finally{
+            try {
+                dbconfig.Con().close();
+                
+            } catch (Exception e) {
+            }
     
+    }
+}    
 }
